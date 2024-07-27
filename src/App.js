d182711805acc1d0eb093f6e5db96588afd02d33
karansinghrautela2001 <karnrautela1@gmail.com>
2024-07-27 15:00:46 +0530

    new

--- src/App.js
+++ src/App.js
@@ -2,13 +2,36 @@
 import './App.css';
 import Navbar from './Navbar';
 import Form from './Form';
+import Alert from './Alr'
+import { useState } from 'react';
 
 function App() {
+   const [alert, setAlert] = useState(null)
+   const showalert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+   }
+  const [mode, setmode]=useState('light')
+  
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setmode('dark')
+      showalert("dark mode is enabled","success")
+    }
+    else{
+      setmode('light')
+      showalert("light mode is enabled","success")
+    }
+  }
+  
   return(
     <>
     
-    <Navbar title="textutils" about="about us"/>
-    <div className=''><Form heading='convert'/></div>
+    <Navbar title="textutils" about="about us" mode={mode} toggleMode={toggleMode}  modee="enable dark mode" />
+    <Alert mode={mode} alert={alert}/>
+    <Form heading='convert' mode={mode}/>
     
    
    
--- src/Form.js
+++ src/Form.js
@@ -1,12 +1,38 @@
 import React from 'react'
 import {useState} from 'react'
 
+
+
 export default function Form(props) {
+  const [myStyle,setMyStyle]=useState({
+    color:'black',
+    backgroundColor:'white'
+  })
+  const[btext,setMyText]=useState('dark mode')
+  const d=()=>{
+    if(myStyle.color === 'black'){
+      setMyStyle({
+        color:'white',
+        backgroundColor:'black',
+        outerHeight:'100%'
+      })
+      setMyText('light mode')
+    }
+    else{
+      setMyStyle({
+        color:'black',
+        backgroundColor:'white'
+      })
+      setMyText('dark mode')
+    }
+  }
+
+
+
 const a=()=>{
 console.log("upper case was clicked")
 let n=text.toUpperCase()
 setText(n)
-
 }
 const c=()=>{
   let n=text.toLowerCase()
@@ -16,16 +42,20 @@ const b=(event)=>{
   console.log("change")
   setText(event.target.value)
 }
-  const[text,setText]=useState('enter here');
+
+const[text,setText]=useState('enter here');
  
   return (
     <>
+   
+    <div style={{backgroundColor:props.mode==='dark'?'black':'white',color:props.mode==='light'?'black':'white',height:800}}>
     <h1 className='container'>{props.heading}</h1>
     <div className=" container mb-3">
   
-  <textarea className="form-control" value={text} onChange={b} id="box" rows="6"></textarea>
-  <button className='btn btn-primary my-2 mx-2' onClick={a}>upper case</button>
+  <textarea className="form-control" style={myStyle} value={text} onChange={b} id="box" rows="6"></textarea>
+  <button className='btn btn-primary my-2 mx-2'  onClick={a}>upper case</button>
   <button className='btn btn-primary ' onClick={c}>lower case</button>
+  <button className='btn btn-primary mx-2' onClick={d}>{btext}</button>
 </div>
 <div className='container my-3'>
   <h1>Your text summary</h1>
@@ -34,6 +64,7 @@ const b=(event)=>{
   <h2>preview
   </h2>
   <p>{text}</p>
+</div>
 </div>
  </>
 )
